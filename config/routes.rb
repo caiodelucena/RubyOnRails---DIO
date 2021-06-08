56e8d34b15100a552bdfcc4f646358101f18c7bc
caiodelucena <caiodelucena6@gmail.com>
2021-06-07 22:53:15 -0300

    crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :products
+  resources :restaurants
+  devise_for :users
+  root 'home#index' 
+  # colocar root 'home#index' vira página
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
